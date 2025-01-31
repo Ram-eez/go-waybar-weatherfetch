38311143abdaf71da9b067d634a93c8864046e1e
Ram-eez <rameez2838@gmail.com>
2025-01-31 17:22:44 +0530

    structs for current weather

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type WeatherResponse struct {
+	Location struct {
+		Name string `json:"name"`
+	} `json:"location"`
+
+	Current struct {
+		TempCurrent string `json:"temp_c"`
+	} `json:"current"`
+}
 
 func main() {
-	fmt.Println("fetch that weather")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	apiKey := os.Getenv("API_KEY")
+
+	url := fmt.Sprintf("http://api.weatherapi.com/v1/current.json?key=%s&q=Srinagar&aqi=yes", apiKey)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	weatherData, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(weatherData))
 }
